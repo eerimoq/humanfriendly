8401a2be4a956a976b6038283f7b2265bd58c714
Erik Moqvist <erik.moqvist@gmail.com>
2019-11-17 09:36:39 +0100

    Initial.

---
+++ README.rst
@@ -0,0 +1,13 @@
+|buildstatus|_
+|codecov|_
+
+About
+=====
+
+Human friendly C library.
+
+.. |buildstatus| image:: https://travis-ci.org/eerimoq/humanfriendly.svg?branch=master
+.. _buildstatus: https://travis-ci.org/eerimoq/humanfriendly
+
+.. |codecov| image:: https://codecov.io/gh/eerimoq/humanfriendly/branch/master/graph/badge.svg
+.. _codecov: https://codecov.io/gh/eerimoq/humanfriendly
---
+++ tst/main.c
@@ -0,0 +1,66 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Erik Moqvist
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * This file is part of the humanfriendly project.
+ */
+
+#include <pwd.h>
+#include "hf.h"
+#include "nala.h"
+#include "nala_mocks.h"
+
+TEST(test_get_username_ok)
+{
+    char buf[32];
+    struct passwd passwd;
+
+    geteuid_mock_once(5);
+    passwd.pw_name = "foobar";
+    getpwuid_mock_once(5, &passwd);
+
+    ASSERT_EQ(hf_get_username(&buf[0], sizeof(buf), ""), &buf[0]);
+    ASSERT_EQ(&buf[0], "foobar");
+}
+
+TEST(test_get_username_default_null)
+{
+    char buf[32];
+
+    geteuid_mock_once(4);
+    getpwuid_mock_once(4, NULL);
+
+    ASSERT_EQ((void *)hf_get_username(&buf[0], sizeof(buf), NULL), NULL);
+}
+
+TEST(test_get_username_default)
+{
+    char buf[32];
+
+    geteuid_mock_once(4);
+    getpwuid_mock_once(4, NULL);
+
+    ASSERT_EQ(hf_get_username(&buf[0], sizeof(buf), "unknown"), &buf[0]);
+    ASSERT_EQ(&buf[0], "unknown");
+}
